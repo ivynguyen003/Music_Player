66a3496f15644d857bcb478821f26336739ad69e
Ivy Nguyen <ivynguyen003@gmail.com>
2018-04-01 00:31:51 -0700

    add artist and release-info

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('#album-cover-art').attr("src", album.albumArtUrl);
+  $('#release-info').text(album.releaseInfo);
+  $('.artist').text(album.artist);
 }
